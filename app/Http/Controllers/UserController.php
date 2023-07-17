654f4ad5232f8e07007efd55b79b5906d232088d
farukwebcode21 <farukahmadmk@yahoo.com>
2023-07-18 00:25:20 +0600

    start laravel bakery posh shop

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+
+class UserController extends Controller {
+    public function UserRegistration(Request $request) {
+        try {
+            User::create([
+                'firstName' => $request->input('firstName'),
+                'lastName'  => $request->input('lastName'),
+                'email'     => $request->input('email'),
+                'mobile'    => $request->input('mobile'),
+                'password'  => $request->input('password'),
+            ]);
+            return response()->json([
+                'status'  => 'success',
+                'message' => 'User Registration Successfully',
+            ], status: 200);
+        } catch (Exception $th) {
+            return response()->json([
+                'status'  => 'faild',
+                'message' => 'User Registration Faild',
+            ], status: 501);
+        }
+
+    }
+}
